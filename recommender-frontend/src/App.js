ab066966fe625a36bf87a1827cd774fe3c9daebc
bsolovij-ebsco <bsolovij@ebsco.com>
2024-03-05 05:57:47 -0500

    adds some static view content

--- recommender-frontend/src/App.js
+++ recommender-frontend/src/App.js
@@ -5,28 +5,14 @@ import Axios from "axios";
 export default function App() {
   return (
     <div>
-      <h1>Basic Example</h1>
-
-      <p>
-        This example demonstrates some of the core features of React Router
-        including nested <code>&lt;Route&gt;</code>s,{" "}
-        <code>&lt;Outlet&gt;</code>s, <code>&lt;Link&gt;</code>s, and using a
-        "*" route (aka "splat route") to render a "not found" page when someone
-        visits an unrecognized URL.
-      </p>
-
       {/* Routes nest inside one another. Nested route paths build upon
           parent route paths, and nested route elements render inside
           parent route elements. See the note about <Outlet> below. */}
       <Router>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="about" element={<About />} />
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="call-the-web" element={<CallTheWeb />} />
-            <Route path="model-attributes" element={< ModelAttributes />} />
-
+            <Route index element={<Search />} />
+            <Route path="results" element={<Results />} />
             {/* Using path="*"" means "match anything", so this route
                 acts like a catch-all for URLs that we don't have explicit
                 routes for. */}
@@ -43,30 +29,7 @@ function Layout() {
     <div>
       {/* A "layout route" is a good place to put markup you want to
         share across all the pages on your site, like navigation. */}
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/call-the-web">Call The Web</Link>
-          </li>
-          <li>
-            <Link to="/model-attributes">MODEL ATTRIBS</Link>
-          </li>
-          <li>
-            <Link to="/nothing-here">Nothing Here</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <hr />
+      
 
       {/* An <Outlet> renders whatever child route is currently active,
         so you can think about this <Outlet> as a placeholder for
@@ -76,80 +39,138 @@ function Layout() {
   );
 }
 
-function Home() {
+function Search() {
+  const [inputText, setInputText] = useState('');
+
+  // Event handler for input changes
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+  };
+
   return (
     <div>
-      <h2>Home</h2>
-      <aside>I am a very fancy home page, don't you think? I can even do math! 2 + 2 = {2+2}</aside>
+      <h1>Welcome to [APP NAME]</h1>
+      <p>Simply enter an author name or book title you enjoy and we'll do our best to recommend you some books!</p>
+      <input
+        type="text"
+        placeholder="Search by author or title"
+        value={inputText}
+        onChange={handleInputChange}
+      />
+
+      {/* Display the entered text */}
+      <a href="/Results"><button onClick={() => {}}>Recommend me some books!</button></a>
     </div>
   );
 }
 
-function About() {
+function Results(){
   return (
     <div>
-      <h2>About</h2>
-      <aside>Here is some information about this page: I don't like sharing too much personal information, so this `About` page is empty. </aside>
+      <h1>Results</h1>
+      <p>Here are your results:</p>
+      <table>
+    <thead>
+      <tr>
+        <th>ISBN</th>
+        <th>Title</th>
+        <th>Author</th>
+        <th>Publish Date</th>
+        <th>Number of Reviews</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>978-0-306-40615-7</td>
+        <td>The Hitchhiker's Guide to the Galaxy</td>
+        <td>Douglas Adams</td>
+        <td>1979-10-12</td>
+        <td>1200</td>
+      </tr>
+      <tr>
+        <td>978-0-439-02349-0</td>
+        <td>Harry Potter and the Sorcerer's Stone</td>
+        <td>J.K. Rowling</td>
+        <td>1997-06-26</td>
+        <td>2500</td>
+      </tr>
+      <tr>
+        <td>978-0-061-19601-1</td>
+        <td>To Kill a Mockingbird</td>
+        <td>Harper Lee</td>
+        <td>1960-07-11</td>
+        <td>1800</td>
+      </tr>
+      <tr>
+        <td>978-0-553-21311-0</td>
+        <td>1984</td>
+        <td>George Orwell</td>
+        <td>1949-06-08</td>
+        <td>1500</td>
+      </tr>
+      <tr>
+        <td>978-1-590-30771-5</td>
+        <td>The Da Vinci Code</td>
+        <td>Dan Brown</td>
+        <td>2003-03-18</td>
+        <td>3200</td>
+      </tr>
+    </tbody>
+  </table>
+  <p><a href='/'><button>Return to search...</button></a></p>
     </div>
   );
+
 }
 
-function Dashboard() {
-  return (
-    <div>
-      <h2>Dashboard</h2>
-      <aside> Let's pretend there are charts and graphs here because why not? Its so exciting!</aside>
-    </div>
-  );
-}
 
-function CallTheWeb() {
-  let [content, setContent] = useState("");
-  useEffect(() => {
-    Axios.get("https://i.imgur.com/XkCmUIC.jpeg", { responseType: 'arraybuffer' })
-      .then((response) => {
-        const base64String = btoa(new Uint8Array(response.data).reduce((data, byte) => data + String.fromCharCode(byte), ''));
-        setContent(base64String);
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-  return (
-    <div>
-      <h2>CallTheWeb</h2>
-      <aside> Let's pretend we are calling the web here - plot twist - we are! </aside>
-      <h1>CONTENT!</h1>
-      <div> content..........
-        <img src={`data:image/jpeg;base64, ${content}`} alt="Description" />
-      </div>
-    </div>
-  );
-}
+// function CallTheWeb() {
+//   let [content, setContent] = useState("");
+//   useEffect(() => {
+//     Axios.get("https://i.imgur.com/XkCmUIC.jpeg", { responseType: 'arraybuffer' })
+//       .then((response) => {
+//         const base64String = btoa(new Uint8Array(response.data).reduce((data, byte) => data + String.fromCharCode(byte), ''));
+//         setContent(base64String);
+//         console.log(response);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   }, []);
+//   return (
+//     <div>
+//       <h2>CallTheWeb</h2>
+//       <aside> Let's pretend we are calling the web here - plot twist - we are! </aside>
+//       <h1>CONTENT!</h1>
+//       <div> content..........
+//         <img src={`data:image/jpeg;base64, ${content}`} alt="Description" />
+//       </div>
+//     </div>
+//   );
+// }
 
-function ModelAttributes() {
-  let [content, setContent] = useState(null);
-  useEffect(() => {
-    Axios.get("//localhost:5000/model-attributes")
-      .then((response) => {
+// function ModelAttributes() {
+//   let [content, setContent] = useState(null);
+//   useEffect(() => {
+//     Axios.get("//localhost:5000/model-attributes")
+//       .then((response) => {
         
-        setContent(response.data);
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-  return (
-    <div>
-      <h2>ModelAttributes</h2>
-      <aside> Here are the attributes from the latest trained model we know about:</aside>
+//         setContent(response.data);
+//         console.log(response);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   }, []);
+//   return (
+//     <div>
+//       <h2>ModelAttributes</h2>
+//       <aside> Here are the attributes from the latest trained model we know about:</aside>
       
-      <div style={{'color': 'red'}}>{ content && content.error}</div>
-    </div>
-  );
-}
+//       <div style={{'color': 'red'}}>{ content && content.error}</div>
+//     </div>
+//   );
+// }
 
 function NoMatch() {
   return (
